380612538cff9c9b43b4c5e8fc83fdc347e6464f
yvendruscolo <yvendruscolo@gmail.com>
2019-04-09 22:19:35 -0300

    initial commit

---
+++ src/scl/core.clj
@@ -0,0 +1,28 @@
+(ns scl.core
+  (:require [jsonista.core :as j] ; fastest yet simple json around
+            [clojure.string :as s]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn])
+  (:gen-class))
+
+(defn parse-line "Takes a header and a regex separator to build lines. If there is a header, outputs maps with the header as keys, else arrays."
+  [header sep]
+  (let [build-line (if header #(zipmap header %) identity)]
+    #(->> (s/split % sep)
+          (mapv edn/read-string)
+          build-line
+          j/write-value-as-string)))
+
+(defn -main [file sep & args]
+  (with-open [input (io/reader file)]
+    (let [init (if (= \\ (first sep)) "\\" "") ; to accept regex we need to escape the '\'s
+          sep* (re-pattern (str init (first sep) "\\s?")) ; build regex, with possible space after
+          no-headers? ((into #{} args) "--no-headers") ; check if we want headers
+          lines* (line-seq input)  ; lazy seq of lines
+          header (if no-headers? false (s/split (first lines*) sep*))
+          lines (if no-headers? lines* (rest lines*))
+          build-line (parse-line header sep*)] ; function that takes a line and builds a json line
+          (->> lines
+              (pmap build-line)
+              (mapv println))
+          (System/exit 0))))
